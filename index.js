4a484ff3b0a4320247541913d693d01e8f7d2f21
Karishma <karishma.7022@gmail.com>
2026-05-23 19:44:53 +0530

    step 2: HTTP Headers - show-headers and secret routes

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.get("/",(req,res)=>{
     res.json({message : "Movie Review API is running!"});
 })
 
+app.get("/show-headers",(req,res)=>{
+    res.json(req.headers);
+})
+
+app.get("/secret",(req,res)=>{
+    const token = req.headers["authorization"];
+    if(!token){
+        return res.status(401).json({message : "No token provided!"});
+    }
+    if(token!=="mytoken123"){
+        return res.status(403).json({message : "Wrong token!"});
+    }
+
+    res.json({message : "Welcome! You are authorized!"});
+})
+
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000");
 })
